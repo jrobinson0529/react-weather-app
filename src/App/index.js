61f5f96a3dd88dcd48997165e410a4ea9d6b7ba0
Jesse Robinson <45837967+jrobinson0529@users.noreply.github.com>
2021-04-23 11:48:24 -0500

    styled up a little bit

--- src/App/index.js
+++ src/App/index.js
@@ -16,19 +16,23 @@ function App() {
       getCurrentWeather(city).then((response) => {
         weatherArray.push(response);
         setWeatherArray([...weatherArray]);
-        console.warn([...weatherArray]);
       });
     }
   };
   return (
     <div className='App'>
-        <WeatherCard weatherArray={weatherArray}/>
-        <Form>
-          <FormGroup>
-            <Label for="exampleEmail">City</Label>
-              <Input type="text" name="city" id="searchCity" placeholder="Type in a city name" onKeyPress={handleEnterPress}/>
-          </FormGroup>
-        </Form>
+      <header>
+        <h1 className='header-text'>Weathertrackr</h1>
+      </header>
+        {weatherArray.length > 0 ? <WeatherCard weatherArray={weatherArray}/> : <h4>Add a city!</h4>}
+        <div className='form-container'>
+          <Form>
+            <FormGroup>
+              <Label for="exampleEmail">Enter your city!</Label>
+                <Input type="text" name="city" id="searchCity" placeholder="Type in a city name" onKeyPress={handleEnterPress}/>
+            </FormGroup>
+          </Form>
+        </div>
     </div>
   );
 }
--- src/helpers/data/weatherData.js
+++ src/helpers/data/weatherData.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
+import apiKeys from '../apiKeys';
 
+const key = apiKeys.weatherApiKey;
 const getCurrentWeather = (city) => new Promise((resolve, reject) => {
-  axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=f290c4061d9be341254dbcbbe6a907fc&units=imperial`)
+  axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}&units=imperial`)
     .then((response) => resolve(response.data))
     .catch((error) => reject(error));
 });
